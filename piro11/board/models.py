4570bc4fb9d8a58573824d2917da8d25e9ba67c9
Inseong Hwang <insung151@naver.com>
2019-08-02 23:57:38 +0900

    Add article model

--- piro11/board/models.py
+++ piro11/board/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    author = models.CharField(max_length=20)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    file = models.FileField(blank=True, null=True)
+    category = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
